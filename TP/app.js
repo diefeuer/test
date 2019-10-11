14500db6d1fdf49257bce48d897e1cf216cbeae4
Mauricio Gomez <mau.gomez.21@gmail.com>
2019-10-11 13:41:40 -0300

    se agrega upper and power case()

---
+++ TP/app.js
@@ -0,0 +1,14 @@
+//creación funcion saludar, parametro es nombre
+//convertis parametro en string
+function saludar(nombre) {
+    console.log("Hola " + nombre);
+}
+//creacion de objeto
+var Wolverine = {
+    nombre: "Logan"
+};
+//var nombre = "Logan";
+/**funcion saludar devuelve objeto con atribo string nombre= Logan  */
+saludar(Wolverine.nombre.toUpperCase());
+// saludar(Wolverine.edad);
+saludar(Wolverine.nombre.toLowerCase());
---
+++ TP/app.ts
@@ -0,0 +1,16 @@
+//creación funcion saludar, parametro es nombre
+//convertis parametro en string
+function saludar(nombre:string) {
+    console.log("Hola " + nombre);
+}
+//creacion de objeto
+var Wolverine = {
+    nombre: "Logan"
+};
+
+//var nombre = "Logan";
+
+/**funcion saludar devuelve objeto con atribo string nombre= Logan  */
+saludar(Wolverine.nombre.toUpperCase());
+// saludar(Wolverine.edad);
+saludar(Wolverine.nombre.toLowerCase());
