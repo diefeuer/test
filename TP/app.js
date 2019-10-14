b3460ceaae92ff1962acd3ea47bf06ac68f1e0b7
Mauricio Gomez <mau.gomez.21@gmail.com>
2019-10-14 14:56:47 -0300

    Funciones de Flecha

--- TP/app.js
+++ TP/app.js
@@ -1,15 +1,58 @@
 "use strict";
-function activar(quien, momento, objeto) {
-    if (objeto === void 0) { objeto = "batiseñal"; }
-    var mensaje;
-    //objeto = "La Baticuea'";
-    if (momento) {
-        mensaje = quien + " RIICCAAA : " + objeto + " en la " + momento;
+var miFuncion = function (a) {
+    return a;
+};
+var miFuncionF = function (a) { return a; };
+console.log(miFuncion("Normal"));
+console.log(miFuncionF("Flecha"));
+var miFuncion2 = function (a, b) {
+    return a + b;
+};
+var miFuncion2F = function (a, b) { return a + b; };
+console.log(miFuncion2(2, 3));
+console.log(miFuncion2F(3, 4));
+var miFuncion3 = function (nombre) {
+    nombre = nombre.toUpperCase();
+    return nombre;
+};
+var miFuncion3F = function (nombre) { return nombre = nombre.toUpperCase(); };
+;
+var miFuncion3F2 = function (nombre) {
+    nombre = nombre.toUpperCase();
+    return nombre;
+};
+console.log(miFuncion3("george"));
+console.log(miFuncion3F("iphone"));
+console.log(miFuncion3F2("murio"));
+// let hulk = {
+//     nombre: "hulk",
+//     smash() {
+//         console.log(this.nombre+ " smash !!");
+//     }
+// }
+//funcion sin flecha el this. apunta afuera del objeto obj1 
+var nombre2 = "bruce1";
+var obj1 = {
+    nombre2: "hulk1",
+    metodo: function () {
+        setTimeout(function () {
+            console.log(this.nombre2 + (" Smash2!!"));
+        }, 1500);
+        console.log(this.nombre2 + (" Smash1!!"));
     }
-    else {
-        mensaje = quien + " RRIICCA : " + objeto;
+};
+//funcion sin flecha el this. dentro afuera del objeto obj1 
+var nombre3 = "bruce2";
+var obj2 = {
+    nombre3: "hulk2",
+    metodo: function () {
+        var _this = this;
+        setTimeout(function () {
+            console.log(_this.nombre3 + (" Smash3!!"));
+        }, 1500);
+        console.log(this.nombre3 + (" Smash4!!"));
     }
-    console.log(mensaje);
-}
-activar("Gorda");
-activar("Flaco", "noche");
+};
+//setTimeout( ()=> console.log(this.nombre2 + " smash!!"), 1500);
+obj1.metodo();
+obj2.metodo();
--- TP/app.ts
+++ TP/app.ts
@@ -1,20 +1,74 @@
-function activar(
-        quien:string, 
-        momento?:string,
-        objeto:string = "batiseñal" 
-        ) {
-  let mensaje: string;
-    //objeto = "La Baticuea'";
-  if(momento){
-    mensaje = `${quien} RIICCAAA : ${objeto} en la ${ momento }`;
-  }else{
-    mensaje = `${quien} RRIICCA : ${objeto}`;
-  }
-  
+let miFuncion = function(a){
+    return a;
+}
+let miFuncionF = (a) => a;
 
-  console.log(mensaje);
+console.log(miFuncion("Normal"));
+console.log(miFuncionF("Flecha"));
+
+
+let miFuncion2 = function (a:number, b:number){
+    return a + b;
+}
+let miFuncion2F = (a:number,b:number) => a + b;
+
+console.log(miFuncion2(2,3));
+console.log(miFuncion2F(3,4));
+
+let miFuncion3 = function (nombre:string){
+    nombre = nombre.toUpperCase();
+    return nombre;
+}
+let miFuncion3F = (nombre:string) => nombre = nombre.toUpperCase();;
+let miFuncion3F2= (nombre:string) => {
+    nombre = nombre.toUpperCase();
+    return nombre;
 }
 
-activar("Gorda");
+console.log(miFuncion3("george"));
+console.log(miFuncion3F("iphone"));
+console.log(miFuncion3F2("murio"));
 
-activar("Flaco","noche")
+// let hulk = {
+//     nombre: "hulk",
+//     smash() {
+//         console.log(this.nombre+ " smash !!");
+        
+//     }
+// }
+
+//funcion sin flecha el this. apunta afuera del objeto obj1 
+let nombre2 = "bruce1";
+
+let obj1 = {
+        nombre2 : "hulk1",
+    metodo(){
+        setTimeout( function(){
+            console.log(this.nombre2 + (" Smash2!!"));
+        }, 1500);
+        console.log(this.nombre2 + (" Smash1!!"));
+        
+    }
+}
+
+//funcion sin flecha el this. dentro afuera del objeto obj1 
+let nombre3 = "bruce2";
+
+let obj2 = {
+        nombre3 : "hulk2",
+    metodo(){
+        setTimeout( () => {
+            console.log(this.nombre3 + (" Smash3!!"));
+        }, 1500);
+        console.log(this.nombre3 + (" Smash4!!"));
+        
+    }
+}
+
+
+
+//setTimeout( ()=> console.log(this.nombre2 + " smash!!"), 1500);
+
+obj1.metodo();
+
+obj2.metodo();
