df30da50c29743d284a3ad31b4eabc9b5faf133d
Mauricio Gomez <mau.gomez.21@gmail.com>
2019-10-14 17:16:19 -0300

    Destructuracion de clases y arreglos

--- TP/app.js
+++ TP/app.js
@@ -1,58 +1,17 @@
 "use strict";
-var miFuncion = function (a) {
-    return a;
+var avenger = {
+    nombre: "Steve",
+    clave: "Capitan America",
+    poder: "droga"
 };
-var miFuncionF = function (a) { return a; };
-console.log(miFuncion("Normal"));
-console.log(miFuncionF("Flecha"));
-var miFuncion2 = function (a, b) {
-    return a + b;
-};
-var miFuncion2F = function (a, b) { return a + b; };
-console.log(miFuncion2(2, 3));
-console.log(miFuncion2F(3, 4));
-var miFuncion3 = function (nombre) {
-    nombre = nombre.toUpperCase();
-    return nombre;
-};
-var miFuncion3F = function (nombre) { return nombre = nombre.toUpperCase(); };
-;
-var miFuncion3F2 = function (nombre) {
-    nombre = nombre.toUpperCase();
-    return nombre;
-};
-console.log(miFuncion3("george"));
-console.log(miFuncion3F("iphone"));
-console.log(miFuncion3F2("murio"));
-// let hulk = {
-//     nombre: "hulk",
-//     smash() {
-//         console.log(this.nombre+ " smash !!");
-//     }
-// }
-//funcion sin flecha el this. apunta afuera del objeto obj1 
-var nombre2 = "bruce1";
-var obj1 = {
-    nombre2: "hulk1",
-    metodo: function () {
-        setTimeout(function () {
-            console.log(this.nombre2 + (" Smash2!!"));
-        }, 1500);
-        console.log(this.nombre2 + (" Smash1!!"));
-    }
-};
-//funcion sin flecha el this. dentro afuera del objeto obj1 
-var nombre3 = "bruce2";
-var obj2 = {
-    nombre3: "hulk2",
-    metodo: function () {
-        var _this = this;
-        setTimeout(function () {
-            console.log(_this.nombre3 + (" Smash3!!"));
-        }, 1500);
-        console.log(this.nombre3 + (" Smash4!!"));
-    }
-};
-//setTimeout( ()=> console.log(this.nombre2 + " smash!!"), 1500);
-obj1.metodo();
-obj2.metodo();
+//traer info de objeto en una linea (*destructuración)
+//{poder:string} = pasa a ser un alias.
+var nombre = avenger.nombre, clave = avenger.clave, poder = avenger.poder;
+//traer info de objeto
+// let nombre = avenger.nombre;
+// let clave = avenger.clave;
+// let poder = avenger.poder;
+console.log(nombre, poder, clave);
+var superHeroes = ["thor", "hulk", "tony"];
+var a = superHeroes[0], b = superHeroes[1], c = superHeroes[2];
+console.log(a, b, c);
--- TP/app.ts
+++ TP/app.ts
@@ -1,74 +1,24 @@
-let miFuncion = function(a){
-    return a;
-}
-let miFuncionF = (a) => a;
 
-console.log(miFuncion("Normal"));
-console.log(miFuncionF("Flecha"));
-
-
-let miFuncion2 = function (a:number, b:number){
-    return a + b;
-}
-let miFuncion2F = (a:number,b:number) => a + b;
-
-console.log(miFuncion2(2,3));
-console.log(miFuncion2F(3,4));
-
-let miFuncion3 = function (nombre:string){
-    nombre = nombre.toUpperCase();
-    return nombre;
-}
-let miFuncion3F = (nombre:string) => nombre = nombre.toUpperCase();;
-let miFuncion3F2= (nombre:string) => {
-    nombre = nombre.toUpperCase();
-    return nombre;
+let avenger = {
+    nombre : "Steve",
+    clave: "Capitan America",
+    poder : "droga"
 }
 
-console.log(miFuncion3("george"));
-console.log(miFuncion3F("iphone"));
-console.log(miFuncion3F2("murio"));
+//traer info de objeto en una linea (*destructuración)
+//{poder:string} = pasa a ser un alias.
+let {nombre,clave,poder} = avenger;
 
-// let hulk = {
-//     nombre: "hulk",
-//     smash() {
-//         console.log(this.nombre+ " smash !!");
-        
-//     }
-// }
-
-//funcion sin flecha el this. apunta afuera del objeto obj1 
-let nombre2 = "bruce1";
-
-let obj1 = {
-        nombre2 : "hulk1",
-    metodo(){
-        setTimeout( function(){
-            console.log(this.nombre2 + (" Smash2!!"));
-        }, 1500);
-        console.log(this.nombre2 + (" Smash1!!"));
-        
-    }
-}
-
-//funcion sin flecha el this. dentro afuera del objeto obj1 
-let nombre3 = "bruce2";
-
-let obj2 = {
-        nombre3 : "hulk2",
-    metodo(){
-        setTimeout( () => {
-            console.log(this.nombre3 + (" Smash3!!"));
-        }, 1500);
-        console.log(this.nombre3 + (" Smash4!!"));
-        
-    }
-}
+//traer info de objeto
+// let nombre = avenger.nombre;
+// let clave = avenger.clave;
+// let poder = avenger.poder;
 
 
+console.log(nombre,poder, clave);
 
-//setTimeout( ()=> console.log(this.nombre2 + " smash!!"), 1500);
+let superHeroes:string[] = ["thor","hulk","tony"];
 
-obj1.metodo();
+let [ a, b, c ] = superHeroes;
 
-obj2.metodo();
+console.log(a,b,c);
